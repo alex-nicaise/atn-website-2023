0d427d44379fce5cc9eb8ff2379de8cdcf37ed6a
alex-nicaise <atnicais@gmail.com>
2023-12-11 15:40:13 -0500

    Added Language icons and styled.

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -1,10 +1,41 @@
+import { Html5Original,
+        BootstrapOriginal,
+        SassOriginal,
+        WordpressPlain,
+        JavascriptOriginal,
+        TypescriptOriginal,
+        ReactOriginal } from 'devicons-react';
+
+
 const Tools = ():React.JSX.Element => {
 
     return (
         <>
             <h1>Tools and Languages</h1>
 
-            <p>Find them here.</p>
+            <div id="programming-icons">
+                <div className="pi-flex-container">
+                    <Html5Original/>
+                </div>
+                <div className="pi-flex-container">
+                    <BootstrapOriginal/>
+                </div>
+                <div className="pi-flex-container">
+                    <SassOriginal/>
+                </div>
+                <div className="pi-flex-container">
+                    <WordpressPlain/>
+                </div>
+                <div className="pi-flex-container">
+                    <JavascriptOriginal/>
+                </div>
+                <div className="pi-flex-container">
+                    <TypescriptOriginal/>
+                </div>
+                <div className="pi-flex-container">
+                    <ReactOriginal/>
+                </div>
+            </div>
         </>
     );
 }
