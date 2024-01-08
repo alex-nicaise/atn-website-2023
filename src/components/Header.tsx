91c78e3e9dbe2f194bfb6c7968b9bf405eab7a8d
alex-nicaise <atnicais@gmail.com>
2024-01-08 16:52:01 -0500

    Fixed CSS viewport issues.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,18 +18,6 @@ const Header = (): React.JSX.Element => {
 
   return (
     <header>
-      {/* META TAGS */}
-      <meta name="og:title" content="Alex Nicaise Portfolio Website"/>
-      <meta name="og:image" content="/nicaise-logo-LARGE-V2.png"/>
-      <meta name="author" content="Alexander Nicaise"/>
-      <meta name="language" content="english"/>
-      <meta charSet="UTF-8"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <meta name="HandheldFriendly" content="true"/>
-      <meta name="format-detection" content="telephone=yes"/>
-      <meta name="copyright" content="Copyright 2023"></meta>
-      <meta name="robots" content="index,follow" />
-
       <img src={Logo} alt="Alex Nicaise Logo"/>
 
 
