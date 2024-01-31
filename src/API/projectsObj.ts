a0cfcead3222dece9c1f0ed1d0d1af230835b398
alex-nicaise <atnicais@gmail.com>
2024-01-31 17:12:33 -0500

    Updated Project to Weathur App v2

--- src/API/projectsObj.ts
+++ src/API/projectsObj.ts
@@ -1,30 +1,32 @@
 const projectsObj = [
-    {
-        title: "Little Lemon Reserve App",
-        imgUrl: "/proj-screenshots/little-lemon-scsh.png",
-        demoUrl: "https://warm-kheer-56c697.netlify.app/",
-        srcCodeUrl: "https://github.com/alex-nicaise/ll-reserve-app"
-    },
-    {
-        title: "Weathur Forecast App",
-        imgUrl: "/proj-screenshots/weathur-scsh.png",
-        demoUrl: "https://ubiquitous-basbousa-ae533a.netlify.app/",
-        srcCodeUrl: "https://github.com/alex-nicaise/theme-weather-app"
-    },
-    {
-        title: "CSICon 2020",
-        imgUrl: "/proj-screenshots/csicon-scsh.png",
-        demoUrl: "https://csiconference.org/",
-        srcCodeUrl: "https://github.com/alex-nicaise/csicon-2020-react-build",
-        notes: "*This website has changed in apppearance since I last worked on it."
-    },
-    {
-        title: "CFI Libraries Website",
-        imgUrl: "/proj-screenshots/cfi-libraries-scsh.png",
-        demoUrl: "https://cfilibraries.org/",
-        srcCodeUrl: "https://github.com/alex-nicaise/cfi-libraries",
-        notes: "*This website has changed in apppearance since I last worked on it."
-    }
-]
+  {
+    title: "Little Lemon Reserve App",
+    imgUrl: "/proj-screenshots/little-lemon-scsh.png",
+    demoUrl: "https://warm-kheer-56c697.netlify.app/",
+    srcCodeUrl: "https://github.com/alex-nicaise/ll-reserve-app",
+  },
+  {
+    title: "Weathur Forecast App",
+    imgUrl: "/proj-screenshots/weathur-scsh.png",
+    demoUrl: "https://lucky-daffodil-1977bd.netlify.app/",
+    srcCodeUrl: "https://github.com/alex-nicaise/theme-weather-app",
+  },
+  {
+    title: "CSICon 2020",
+    imgUrl: "/proj-screenshots/csicon-scsh.png",
+    demoUrl: "https://csiconference.org/",
+    srcCodeUrl: "https://github.com/alex-nicaise/csicon-2020-react-build",
+    notes:
+      "*This website has changed in apppearance since I last worked on it.",
+  },
+  {
+    title: "CFI Libraries Website",
+    imgUrl: "/proj-screenshots/cfi-libraries-scsh.png",
+    demoUrl: "https://cfilibraries.org/",
+    srcCodeUrl: "https://github.com/alex-nicaise/cfi-libraries",
+    notes:
+      "*This website has changed in apppearance since I last worked on it.",
+  },
+];
 
 export default projectsObj;
